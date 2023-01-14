9e734a425c445f89c6f1dec4c8abef1df4ceb68e
Sebastián Giraldo <joabgiraldo@gmail.com>
2023-01-14 09:30:37 -0500

    Initial Commits

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,18 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
-      <body>
+    <Html lang="es">
+      <Head>
+        <title>Linktree - Clone</title>
+        <meta name="description" content="My clone of Linktree" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <link rel="icon" href="/logo.svg" />
+      </Head>
+      <body className="bg-[conic-gradient(at_top,_var(--tw-gradient-stops))] from-sky-500 via-orange-200 to-yellow-600">
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
