143955bd6f0ca195ea4ddc036114cb1833c4a36d
Sebastián Giraldo <joabgiraldo@gmail.com>
2023-01-14 11:24:45 -0500

    Design + Links + Responsive

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,12 +3,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="es">
-      <Head>
-        <title>Linktree - Clone</title>
-        <meta name="description" content="My clone of Linktree" />
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-        <link rel="icon" href="/logo.svg" />
-      </Head>
+      <Head />
       <body className="bg-[conic-gradient(at_top,_var(--tw-gradient-stops))] from-sky-500 via-orange-200 to-yellow-600">
         <Main />
         <NextScript />
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,27 @@
 import Image from "next/image";
 import data from "../data.json";
 import { BiLinkExternal } from "react-icons/bi";
+import Head from "next/head";
 
 function LinkCard({
   href,
   title,
-  description,
   image,
+  color,
 }: {
   href: string;
   title: string;
-  description: string;
   image: string;
+  color: boolean;
 }) {
   return (
     <a
+      no-referrer="true"
+      target="_blank"
       href={href}
-      className="bg-white flex items-center p-1 rounded-md shadow-lg hover:scale-105 transition-all border border-gray-300 mb-1"
+      className={`${
+        color ? "bg-[#FFF2D7]" : "bg-white"
+      } flex items-center p-1 rounded-md shadow-lg hover:scale-105 transition-all mb-1`}
     >
       <div className="w-full flex justify-between items-center pr-2">
         {image && (
@@ -36,30 +41,65 @@ function LinkCard({
   );
 }
 
+function LinkSocial({ href, icon }: { href: string; icon: string }) {
+  return (
+    <a no-referrer="true" target="_blank" href={href} className="hover:scale-110 transition-all">
+      <Image
+        src={icon}
+        alt="Icon"
+        width={40}
+        height={40}
+        className="pointer-events-none"
+      />
+    </a>
+  );
+}
+
 export default function Home() {
   return (
-    <div className="h-screen flex flex-col items-center justify-between mx-auto w-2xl max-w-[680px] px-4 py-16">
-      <div className="w-full flex flex-col">
-        <div className="flex justify-center">
-          <Image
-            className="rounded-full"
-            src={data.avatar}
-            alt={data.name}
-            width={96}
-            height={96}
-          />
-        </div>
-        <h1 className="font-bold m-0 mt-4 text-xl overflow-hidden whitespace-nowrap text-center">
-          {data.name}
-        </h1>
+    <>
+      <Head>
+        <title>Linktree - Clone</title>
+        <meta name="description" content="My clone of Linktree" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap"
+          rel="stylesheet"
+        />
+        <link rel="icon" href="/favicon.svg" />
+      </Head>
+      <div className="h-screen flex flex-col items-center justify-between mx-auto w-2xl max-w-[680px] px-4 py-16">
+        <div className="w-full flex flex-col">
+          <div className="flex justify-center">
+            <Image
+              className="rounded-full"
+              src={data.avatar}
+              alt={data.name}
+              width={96}
+              height={96}
+            />
+          </div>
+          <h1 className="font-bold m-0 mt-4 text-xl overflow-hidden whitespace-nowrap text-center">
+            {data.name}
+          </h1>
 
-        <div className="w-full flex flex-col gap-3 mt-8">
-          {data.links.map((link, i) => (
-            <LinkCard key={i} {...link} />
-          ))}
+          <div className="w-full flex flex-col gap-3 mt-8">
+            {data.links.map((link, i) => (
+              <LinkCard key={i} {...link} />
+            ))}
+          </div>
+        </div>
+        <div className="flex flex-col gap-3">
+          <div className="flex gap-3">
+            {data.socials.map((link, i) => (
+              <LinkSocial key={i} {...link} />
+            ))}
+          </div>
+          <h1 className="flex items-center justify-center gap-5 font-bold text-center text-2xl cursor-none hover:scale-105 transition-all">Linktree 
+              <Image src="/logo.svg" alt="Logo" width={50} height={50} className="text-black" />
+          </h1>
         </div>
       </div>
-      <div>a</div>
-    </div>
+    </>
   );
 }
